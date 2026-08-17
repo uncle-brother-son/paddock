c1002889855d70a0d80807bdaffb63d4eb2b3f2e
uncle-brother-son <wayne@unclebrotherson.com>
2026-08-17 22:58:23 +0100

    Fix build: remove Google Fonts, fix async cookies, disable ESLint during builds

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -2,8 +2,8 @@
 import storybook from "eslint-plugin-storybook";
 
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from "eslint-config-next/core-web-vitals.js";
+import nextTs from "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
